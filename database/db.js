d5cd29824fdf2fbde47d868c0fd56b08b445307b
Anshul <malikanshul29@gmail.com>
2016-10-29 08:43:07 +0530

    a

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 /* jshint esversion:6 */
 var MongoClient = require('mongodb').MongoClient;
 var url = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
+console.log(url); 
 class Client {
   constructor() {
     MongoClient.connect(url, (err, database) => {
